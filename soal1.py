150505d94a941c8988829cc3ba33f9d99780d54e
vinmora <jhousin2002@gmail.com>
2022-11-06 00:55:27 +0700

    Soal 1 Final Answer

--- soal1.py
+++ soal1.py
@@ -1,19 +1,8 @@
 import os
 os.system("cls")
 
-
-def hasil(i):
-    x = 0
+for i in range (5,0,-1):
     x = i * (i+1) / 2
-    return x
-
-for i in range (5+1,1,-1):
-    for j in range (i-1,0,-1):
-        print(j,end="")
-    print("= ")
-    for k in range (i-1,0,-1):
-        print(hasil(i),end="")
-        
-  
-
- 
+    for j in range (1,i+1,1):
+        print(j ,end=" ")
+    print("= ", int(x))
