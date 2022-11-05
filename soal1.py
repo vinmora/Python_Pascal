785cf3820c0f434a73abe98de499bfe70927c6fb
vinmora <jhousin2002@gmail.com>
2022-11-05 21:16:45 +0700

    Menambahkan file function

---
+++ soal1.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+for i in range (5+1,1,-1):
+    x = i * (i+1) / 2
+    for j in range (i-1,0,-1):
+        print(j,end=" ")
+    print("= ")
+
+ 
---
+++ testDef.py
@@ -0,0 +1,5 @@
+def tahu(i):
+    print("harga tahu adalah", 20000 * i)
+
+for i in range (1,5):
+    tahu(i)
