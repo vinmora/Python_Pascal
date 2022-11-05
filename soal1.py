181ac91f91351673a99b086d407170a97c2df870
vinmora <jhousin2002@gmail.com>
2022-11-06 00:45:04 +0700

    test 1 soal kompleks

--- soal1.py
+++ soal1.py
@@ -1,10 +1,19 @@
 import os
 os.system("cls")
 
-for i in range (5+1,1,-1):
+
+def hasil(i):
+    x = 0
     x = i * (i+1) / 2
+    return x
+
+for i in range (5+1,1,-1):
     for j in range (i-1,0,-1):
-        print(j,end=" ")
+        print(j,end="")
     print("= ")
+    for k in range (i-1,0,-1):
+        print(hasil(i),end="")
+        
+  
 
  
