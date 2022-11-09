4eb4afcc4eac58967c07c3a66b04f3af46c5fd30
vinmora <jhousin2002@gmail.com>
2022-11-09 18:08:48 +0700

    Ada UTS, Masukkan filenya

--- tempCodeRunnerFile.pas
+++ tempCodeRunnerFile.pas
@@ -1,21 +1,7 @@
-program soal4;
+program soal3;
 uses crt;
 
-var i,x : integer;
-a,b : real;
+var i,n : integer;
+
 begin
 clrscr;
-
-a := 0;
-b := 0;
-write('Masukkan nilai X: ');readln(x);
-
-for i := 1 to x do
-begin
-    a := 5 + (i-1) * 10;
-    b := b + a;
-end;
-write ('Suku ke-:',x,' adalah ',a:5:0,', jumlah seluruh suku X adalah: ',b:5:0);
-
-
-end.
