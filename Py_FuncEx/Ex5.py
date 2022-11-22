454816cfa960c01002509b205c0f79c5c4533eb4
vinmora <jhousin2002@gmail.com>
2022-11-22 12:27:43 +0700

    Function inside function

---
+++ Py_FuncEx/Ex5.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+
+def fungout(a,b):
+    y = a - b
+    def fung(a,b):
+        x = a + b
+        return x
+    
+    z = fung(a,b)
+    return z+5
+
+
+
+y = fungout(20,50)
+print(y)
